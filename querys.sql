36f7c25de8c81412a7ce3bc4e7ad66628020358d
LUIGGY MAMANI CONDORI <luiggy.mamani.12777190@usalesiana.edu.bo>
2022-12-11 12:26:05 -0400

    query and trigger added

--- querys.sql
+++ querys.sql
@@ -1,3 +1,21 @@
 -- Quiero conocer a todos los clientes aprobados con el instructor de teoría llamado ‘Juan Perez’
 SELECT *
 FROM Student_Building
+
+-- First Consultation
+SELECT Student_Important.first_name, Student_Important.last_name, Trainer.first_name, Trainer.last_name, Subject.name
+FROM Student_Building
+inner join Student_Important on Student_Important.id = Student_Building.Student_id
+inner join Building on Building.id = Student_Building.Building_id
+inner join Subject on Subject.id = Building.Subject_id
+inner join Trainer on Trainer.id = Building.Trainer_id
+where Trainer.first_name = "Ariel" and Trainer.last_name = "Blanco";
+
+-- Second Consultation
+SELECT Student_Important.first_name, Student_Important.last_name, Subject.name, Category.type
+FROM Student_Building
+inner join Student_Important on Student_Important.id = Student_Building.Student_id
+inner join Building on Building.id = Student_Building.Building_id
+inner join Subject on Subject.id = Building.Subject_id
+inner join Category on Category.id = Building.Category_id
+where Category.type in ("A", "P");
---
+++ triggers.sql
@@ -0,0 +1,23 @@
+DELIMITER //
+CREATE TRIGGER Save_Notes_History
+AFTER INSERT ON Student_Note FOR EACH ROW
+BEGIN
+	DECLARE nameStudent varchar (45);
+    DECLARE lastNameStudent varchar (45);
+	DECLARE nameSubject varchar (45);
+    DECLARE typeSubject varchar (45);
+    DECLARE nameTrainer varchar (45);
+    DECLARE lastNameTrainer varchar (45);
+
+	SELECT Student_Important.first_name, Student_Important.last_name, Subject.name, Subject.type, Trainer.first_name, Trainer.last_name
+    into nameStudent, lastNameStudent, nameSubject, typeSubject, nameTrainer, lastNameTrainer from Student_Building
+    inner join Student_Important on Student_Important.id = Student_Building.Student_id
+    inner join Building on Building.id = Student_Building.Building_id
+    inner join Subject on Subject.id = Building.Subject_id
+    inner join Trainer on Trainer.id = Building.Trainer_id
+    where Student_Building.id = NEW.Student_Building_id;
+
+	INSERT INTO Note_History (name_student, last_name_student, score, name_subject, type_subject, name_trainer, last_name_trainer, date_time)
+	VALUES (nameStudent, lastNameStudent, NEW.score, nameSubject, typeSubject, nameTrainer, lastNameTrainer, now());
+END //
+DELIMITER ;
