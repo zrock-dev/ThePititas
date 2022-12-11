d689612b4585bdf3b2e7d86f581ae6c07a2ad7ba
Santiago C. Saavedra <santisaavcon+commits@gmail.com>
2022-12-11 13:35:08 -0400

    querys

--- querys.sql
+++ querys.sql
@@ -1,9 +1,9 @@
--- Quiero conocer a todos los clientes aprobados con el instructor de teoría llamado ‘Juan Perez’
+# Quiero conocer a todos los clientes aprobados con el instructor de teoría llamado ‘Juan Perez’
 SET @trainer_id = (SELECT id
                    FROM Trainer
                    WHERE first_name = 'Juan');
 
-EXPLAIN (SELECT SI.first_name, SI.last_name, SI.age
+(SELECT SI.first_name, SI.last_name, SI.age
  FROM Student_Building AS SB
  INNER JOIN Student_Important SI on SI.id = SB.Student_id
  WHERE Building_id
@@ -11,7 +11,7 @@ EXPLAIN (SELECT SI.first_name, SI.last_name, SI.age
    AND (SELECT AVG(score) FROM Student_Note AS S WHERE S.Student_Building_id = SB.Student_id) >= 51);
 
 
--- Agrupar por cada instructor el número de clientes inscritos en cada grupo de la categoría A, inscritos en la fecha 1 al 30 de mayo del 2020
+# Agrupar por cada instructor el número de clientes inscritos en cada grupo de la categoría A, inscritos en la fecha 1 al 30 de mayo del 2020
 SELECT COUNT(SB.Student_id)
 FROM Building AS B
 INNER JOIN Student_Building SB ON B.id = SB.Building_id
