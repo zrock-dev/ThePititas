0e8e604d8493b0505d2e96d7db1bfd990edb7448
Daniel Espinoza <danii003559@gmail.com>
2022-12-11 23:10:09 -0400

    improve data trainer

--- DDL/Tables/Trainer.sql
+++ DDL/Tables/Trainer.sql
@@ -1,8 +1,8 @@
 insert into Trainer (id, first_name, last_name, age, phone, gender, salary)
-values  (1, 'Lonie', 'Beatty', 248, 98, 'M', 1298),
-        (2, 'Anya', 'Corwin', 256, 1, 'M', 555),
-        (3, 'Dana', 'Mueller', 438, 681, 'M', 1133),
-        (4, 'Rickie', 'Langworth', 498, 1, 'M', 1672),
-        (5, 'Rebeka', 'Johns', 304, 500801, 'M', 1069),
-        (6, 'Osvaldo', 'Casper', 273, 0, 'F', 2199),
-        (7, 'Burley', 'Friesen', 399, 2147483647, 'M', 1020);
+values  (1, 'Lonie', 'Beatty', 27, 79367821, 'F', 3298),
+        (2, 'Anya', 'Corwin', 56, 79638212, 'F', 5505),
+        (3, 'Dana', 'Mueller', 38, 68183112, 'F', 4533),
+        (4, 'Rickie', 'Langworth', 28, 67561267, 'M', 4672),
+        (5, 'Rebeka', 'Johns', 24, 700801, 'F', 5069),
+        (6, 'Osvaldo', 'Casper',43, 79368173, 'M', 6199),
+        (7, 'Burley', 'Friesen', 29, 68368612, 'M', 4020);
