7ca1652e5eb9a6f137d76f4d81fd291fb4259578
LUIGGY MAMANI CONDORI <luiggy.mamani.12777190@usalesiana.edu.bo>
2022-12-11 22:25:50 -0400

    added images

---
+++ Table_Queries.sql
@@ -0,0 +1,17 @@
+USE DriverCollege;
+
+SELECT * FROM Company;
+SELECT * FROM Certificate;
+SELECT * FROM Category;
+SELECT * FROM Schedule;
+SELECT * FROM Building_Schedule;
+SELECT * FROM Student;
+SELECT * FROM Student_Building;
+SELECT * FROM Student_Note;
+SELECT * FROM Subject;
+SELECT * FROM Trainer;
+SELECT * FROM Vehicle;
+SELECT * FROM Data_Maintenance;
+SELECT * FROM Vehicle_Building;
+SELECT * FROM Building;
+SELECT * FROM Note_History;
